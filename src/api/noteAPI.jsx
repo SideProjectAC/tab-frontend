9d6f5877bfe9c0f2e276afdf8e542e2e13435362
Jimmy Hsu <jimmyhsu82722@gmail.com>
2024-04-12 14:56:16 +0800

    Add NoteAPI

---
+++ src/api/noteAPI.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const token = localStorage.getItem('authToken')
+
+const api = axios.create({
+  baseURL: 'http://localhost:5050',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+
+export async function PostNewNoteAPI(group_id, newNoteData) {
+  return api.post(`/groups/${group_id}/notes`, newNoteData)
+}
