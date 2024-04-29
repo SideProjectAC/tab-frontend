48e9d311d5616d59f14a46d2254a88d7be7b99b6
Jimmy Hsu <jimmyhsu82722@gmail.com>
2024-04-29 17:07:53 +0800

    feat: Todo item
    1. Auto save item content when switch type
    2. Save doneStatus (backEnd API is on fixing)
    3. Enable edit todo content

--- src/components/main/note.jsx
+++ src/components/main/note.jsx
@@ -13,6 +13,7 @@ function Note({ item, groupId }) {
   const { setGroups } = useGroups()
   const [noteContent, setNoteContent] = useState(item?.note_content || '')
   const [noteType, setNoteType] = useState(item?.item_type || 1) //TODO 需要預設為1就好嗎？
+  const [todoDoneStatus, setTodoDoneStatus] = useState(false)
   const noteBgColor = '#f7f7f7' //暫無變換顏色功能
 
   const handleAddNote = async () => {
@@ -43,14 +44,41 @@ function Note({ item, groupId }) {
     )
     setNoteContent('')
   }
+  const handleChangeItemContent = (event) => {
+    event.key !== 'Enter'
+      ? setNoteContent(event.target.value)
+      : // 編輯 note 內容而不是按下 Enter 鍵
+        (() => {
+          event.preventDefault()
+          // 如果是現存的 item
+          item
+            ? // 如果是 note
+              noteType === 1
+              ? handlePatchNoteContent()
+              : // 如果是 todo
+                handlePatchTodoContent()
+            : // 如果是新的 item
+              handleAddNote()
+        })()
+  }
   const handlePatchNoteContent = async () => {
     const patchNoteContent = { note_content: noteContent }
-    const response = await patchNoteAPI(
-      groupId,
-      item?.item_id,
-      patchNoteContent
-    )
-    console.log(response)
+    try {
+      await patchNoteAPI(groupId, item?.item_id, patchNoteContent)
+      console.log('Patch note content request sent successfully.')
+    } catch (error) {
+      console.log('Error patching note:', error)
+    }
+  }
+  const handlePatchTodoContent = async () => {
+    //todo 現：不管是note還是todo都是改noteContent，應改為改 todoContent（潛在問題：輸入 content 到一半按下切換鈕）
+    const patchTodoContent = { note_content: noteContent }
+    try {
+      await patchTodoAPI(groupId, item?.item_id, patchTodoContent)
+      console.log('Patch todo content request sent successfully.')
+    } catch (error) {
+      console.log('Error patching todo:', error)
+    }
   }
 
   //FIX 這個版本可以快速切換，但是不一定能打出正確 API
@@ -73,8 +101,12 @@ function Note({ item, groupId }) {
   // }
 
   //FIX 這個版本只有正確打出 API 才可以自由切換
-  const handlePatchNoteType = async () => {
+  const handlePatchItemType = async () => {
     try {
+      await noteType === 1
+      ? handlePatchNoteContent()
+      : handlePatchTodoContent()
+
       const newNoteType = noteType === 1 ? 2 : 1
       const patchAPI = noteType === 1 ? patchNoteAPI : patchTodoAPI
       const patchNoteType = { item_type: newNoteType }
@@ -91,17 +123,17 @@ function Note({ item, groupId }) {
       console.log('Error patching item type:', error)
     }
   }
-
-  const handleChangeNote = (event) => {
-    event.key !== 'Enter'
-      ? setNoteContent(event.target.value)
-      : (() => {
-          event.preventDefault()
-          item ? handlePatchNoteContent() : handleAddNote()
-        })()
+  const handlePatchDoneStatus = async () => {
+    const patchDoneStatus = { doneStatus: !todoDoneStatus }
+    try {
+      await patchTodoAPI(groupId, item?.item_id, patchDoneStatus)
+      console.log('Patch done status request sent successfully.')
+      setTodoDoneStatus(!todoDoneStatus)
+    } catch (error) {
+      console.log('Error patching done status:', error)
+    }
   }
-
-  const handleDeleteNote = async (groupId) => {
+  const handleDeleteItem = async (groupId) => {
     setGroups((prev) =>
       prev.map((group) => {
         if (group.group_id === groupId) {
@@ -129,22 +161,27 @@ function Note({ item, groupId }) {
         key={item?.item_id}
         className='noteItem'
         style={{ backgroundColor: item?.note_bgColor }}>
-        {noteType === 2 && <input className='CheckBox' type='checkbox'></input>}
+        {noteType === 2 && (
+          <input
+            className='CheckBox'
+            type='checkbox'
+            onClick={handlePatchDoneStatus}></input>
+        )}
         <textarea
-          className='note'
+          className={noteType === 1 ? 'note' : 'todo'}
           value={noteContent}
-          onChange={handleChangeNote}
-          onKeyDown={handleChangeNote}
+          onChange={handleChangeItemContent}
+          onKeyDown={handleChangeItemContent}
           style={{ backgroundColor: item?.note_bgColor }}></textarea>
         {item && (
           <div>
-            <button className='switchNoteButton' onClick={handlePatchNoteType}>
+            <button className='switchNoteButton' onClick={handlePatchItemType}>
               切換
             </button>
             <button
               className='deleteButton'
               onClick={() => {
-                handleDeleteNote(groupId)
+                handleDeleteItem(groupId)
               }}>
               x
             </button>
