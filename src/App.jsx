451188602abb3087dc369526c2c7baa38f9e7b13
Jimmy Hsu <jimmyhsu82722@gmail.com>
2024-05-21 10:57:05 +0800

    Optimize event listener registration by using useEffect
    - Add useEffect to register and remove storage event listener
    - Prevent repeated listener registration by cleaning up in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import DragDropComponent from "./components/main/Drag";
-import { ChromeTabsProvider } from "./components/useContext/ChromeTabsContext";
-import { GroupsProvider } from "./components/useContext/GroupContext";
-import { ThemeProvider } from "./components/useContext/ThemeContext";
+import { useEffect } from 'react'
+import DragDropComponent from './components/main/drag'
+import { ChromeTabsProvider } from './components/useContext/ChromeTabsContext'
+import { GroupsProvider } from './components/useContext/GroupContext'
+import { ThemeProvider } from './components/useContext/ThemeContext'
 import {
   MemoryRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
-import Login from "./components/login/Login";
-import Register from "./components/login/Register";
+} from 'react-router-dom'
+import Login from './components/login/Login'
+import Register from './components/login/Register'
 
 const App = () => {
-  window.addEventListener("storage", (event) => {
-    localStorage.removeItem("needReload");
-    if (event.key === "needReload" && event.newValue === "true") {
-      window.location.reload();
-      localStorage.removeItem("needReload");
+  useEffect(() => {
+    const reloadPage = (event) => {
+      localStorage.removeItem('needReload')
+      if (event.key === 'needReload' && event.newValue === 'true') {
+        window.location.reload()
+        localStorage.removeItem('needReload')
+      }
     }
-  });
+    window.addEventListener('storage', reloadPage)
+    return () => {
+      window.removeEventListener('storage', reloadPage)
+    }
+  }, [])
 
   return (
     <Router>
@@ -26,16 +33,16 @@ const App = () => {
         <GroupsProvider>
           <ChromeTabsProvider>
             <Routes>
-              <Route path="/main" element={<DragDropComponent />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="*" element={<Navigate to="/login" />} />
+              <Route path='/main' element={<DragDropComponent />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='*' element={<Navigate to='/login' />} />
             </Routes>
           </ChromeTabsProvider>
         </GroupsProvider>
       </ThemeProvider>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/main/handleDragDrop.jsx
+++ src/components/main/handleDragDrop.jsx
@@ -1,74 +1,74 @@
-import { useRef } from "react";
+import { useRef } from 'react'
 import {
   deleteItemFromGroupAPI,
   patchItemToExistingGroupsAPI,
   postTabAPI,
-} from "../../api/itemAPI";
-import { postGroupAPI } from "../../api/groupAPI";
+} from '../../api/itemAPI'
+import { postGroupAPI } from '../../api/groupAPI'
 
 export const useDragDrop = (activeTabs, groups, setGroups, setActiveTabs) => {
-  const originGroupIdRef = useRef();
-  const itemIdRef = useRef();
+  const originGroupIdRef = useRef()
+  const itemIdRef = useRef()
 
   const handleDragStart = (e, itemId, originGroupId) => {
-    e.dataTransfer.setData("itemId", itemId);
-    e.dataTransfer.setData("originGroupId", originGroupId);
-    originGroupIdRef.current = originGroupId;
-    itemIdRef.current = itemId;
-    e.dataTransfer.effectAllowed = "move";
-  };
+    e.dataTransfer.setData('itemId', itemId)
+    e.dataTransfer.setData('originGroupId', originGroupId)
+    originGroupIdRef.current = originGroupId
+    itemIdRef.current = itemId
+    e.dataTransfer.effectAllowed = 'move'
+  }
 
   const handleDragOver = (e) => {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
-  };
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'move'
+  }
 
   const handleDrop = async (e, targetGroupId) => {
-    e.preventDefault();
-    const originGroupId = originGroupIdRef.current;
-    const itemId = itemIdRef.current;
+    e.preventDefault()
+    const originGroupId = originGroupIdRef.current
+    const itemId = itemIdRef.current
     const originGroupIndex = groups.findIndex(
       (group) => group.group_id === originGroupId
-    );
+    )
 
-    if (originGroupId === targetGroupId) return;
+    if (originGroupId === targetGroupId) return
 
-    let draggedItem;
+    let draggedItem
     //前端先刪除原本在的地方
-    if (originGroupId === "ActiveTabs") {
-      draggedItem = activeTabs.find((tab) => tab.browserTab_id === itemId);
+    if (originGroupId === 'ActiveTabs') {
+      draggedItem = activeTabs.find((tab) => tab.browserTab_id === itemId)
       setActiveTabs((prev) =>
         prev.filter((tab) => tab.browserTab_id !== itemId)
-      );
-      closeTab(draggedItem.browserTab_id);
+      )
+      closeTab(draggedItem.browserTab_id)
     } else {
       draggedItem = groups[originGroupIndex].items.find(
         (item) => item.item_id === itemId
-      );
+      )
       updateGroupItems(originGroupId, (items) =>
         items.filter((item) => item.item_id !== itemId)
-      );
+      )
     }
 
     //新增到新的地方
-    if (targetGroupId === "ActiveTabs") {
-      await deleteItemFromGroupAPI(originGroupId, itemId);
-      setActiveTabs((prev) => [...prev, draggedItem]);
-      openTab(draggedItem.browserTab_url);
-      return;
+    if (targetGroupId === 'ActiveTabs') {
+      await deleteItemFromGroupAPI(originGroupId, itemId)
+      setActiveTabs((prev) => [...prev, draggedItem])
+      openTab(draggedItem.browserTab_url)
+      return
     } else {
       const targetGroup = groups.find(
         (group) => group.group_id === targetGroupId
-      );
+      )
       await handleGroupTransfer(
         draggedItem,
         originGroupId,
         targetGroupId,
         targetGroup,
         itemId
-      );
+      )
     }
-  };
+  }
 
   const handleGroupTransfer = async (
     draggedItem,
@@ -79,7 +79,7 @@ export const useDragDrop = (activeTabs, groups, setGroups, setActiveTabs) => {
   ) => {
     try {
       //從ActiveTabs拉到newGroup區域: 後端給新GroupID 和 ItemID
-      if (targetGroupId.current === null && originGroupId === "ActiveTabs") {
+      if (targetGroupId.current === null && originGroupId === 'ActiveTabs') {
         const newGroupTabData = {
           browserTab_favIconURL: draggedItem.browserTab_favIconURL,
           browserTab_title: draggedItem.browserTab_title,
@@ -90,9 +90,9 @@ export const useDragDrop = (activeTabs, groups, setGroups, setActiveTabs) => {
           browserTab_status: draggedItem.browserTab_status,
           windowId: draggedItem.windowId,
           group_icon: randomEmoji(),
-          group_title: "Untitled",
-        };
-        const response = await postGroupAPI(newGroupTabData);
+          group_title: 'Untitled',
+        }
+        const response = await postGroupAPI(newGroupTabData)
         const newGroup = {
           group_icon: newGroupTabData.group_icon,
           group_title: newGroupTabData.group_title,
@@ -104,30 +104,30 @@ export const useDragDrop = (activeTabs, groups, setGroups, setActiveTabs) => {
               item_id: response.data.item_id,
             },
           ],
-        };
-        updateGroups(newGroup);
-        return;
+        }
+        updateGroups(newGroup)
+        return
       }
       //從已從在的group拉到newGroup區域: 後端給新GroupID 但沿用原ItemID
-      if (targetGroupId.current === null && originGroupId !== "ActiveTabs") {
+      if (targetGroupId.current === null && originGroupId !== 'ActiveTabs') {
         const tabData = {
           sourceGroup_id: originGroupId,
           item_id: itemId,
-          group_title: "Untitled",
+          group_title: 'Untitled',
           group_icon: randomEmoji(),
-        };
-        const response = await postGroupAPI(tabData);
+        }
+        const response = await postGroupAPI(tabData)
         const newGroup = {
           group_icon: tabData.group_icon,
           group_title: tabData.group_title,
           group_id: response.data.group_id,
           items: [{ ...draggedItem }],
-        };
-        updateGroups(newGroup);
-        return;
+        }
+        updateGroups(newGroup)
+        return
       }
       // 從ActiveTabs拉到已存在的group,使用post新增該item
-      if (targetGroupId !== undefined && originGroupId === "ActiveTabs") {
+      if (targetGroupId !== undefined && originGroupId === 'ActiveTabs') {
         const tabData = {
           browserTab_favIconURL: draggedItem.browserTab_favIconURL,
           browserTab_title: draggedItem.browserTab_title,
@@ -138,89 +138,89 @@ export const useDragDrop = (activeTabs, groups, setGroups, setActiveTabs) => {
           browserTab_status: draggedItem.browserTab_status,
           windowId: draggedItem.windowId,
           targetItem_position: targetGroup.items.length,
-        };
-        const response = await postTabAPI(targetGroupId, tabData);
+        }
+        const response = await postTabAPI(targetGroupId, tabData)
         const newDraggedTab = {
           ...draggedItem,
           item_type: 0,
           item_id: response.item_id,
-        };
-        updateGroupItems(targetGroupId, (items) => [...items, newDraggedTab]);
-        return;
+        }
+        updateGroupItems(targetGroupId, (items) => [...items, newDraggedTab])
+        return
       }
       //拉到已存在的group,使用patch移動該item
-      if (targetGroupId !== undefined && originGroupId !== "ActiveTabs") {
+      if (targetGroupId !== undefined && originGroupId !== 'ActiveTabs') {
         const targetPosition = {
           targetItem_position: targetGroup.items.length,
           targetGroup_id: targetGroupId,
-        };
+        }
         await patchItemToExistingGroupsAPI(
           originGroupId,
           itemId,
           targetPosition
-        );
-        const newDraggedTab = { ...draggedItem, item_id: itemId };
-        updateGroupItems(targetGroupId, (items) => [...items, newDraggedTab]);
-        return;
+        )
+        const newDraggedTab = { ...draggedItem, item_id: itemId }
+        updateGroupItems(targetGroupId, (items) => [...items, newDraggedTab])
+        return
       }
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   //更新前端Groups（當有newGroup被新增時）
   const updateGroups = (newGroup) => {
     setGroups((prevGroups) => {
-      const updatedGroups = [...prevGroups, newGroup];
-      return updatedGroups;
-    });
-  };
+      const updatedGroups = [...prevGroups, newGroup]
+      return updatedGroups
+    })
+  }
 
   //更新前端Group裡的items
   const updateGroupItems = (groupId, updateFunction) => {
     setGroups((prev) =>
-      prev.map((group) =>
-        group.group_id === groupId
+      prev.map((group) => {
+        return group.group_id === groupId
           ? { ...group, items: updateFunction(group.items) }
           : group
-      )
-    );
-  };
+      })
+    )
+  }
 
   const randomEmoji = () => {
     const emojiList = [
-      "🎀",
-      "⚽",
-      "🎾",
-      "🏁",
-      "😡",
-      "💎",
-      "🚀",
-      "🌙",
-      "🎁",
-      "⛄",
-      "🌊",
-      "⛵",
-      "🏀",
-      "🐷",
-      "🐍",
-      "🐫",
-      "🔫",
-      "🍉",
-      "💛",
-    ];
-    return emojiList[Math.floor(Math.random() * emojiList.length)];
-  };
+      '🎀',
+      '⚽',
+      '🎾',
+      '🏁',
+      '😡',
+      '💎',
+      '🚀',
+      '🌙',
+      '🎁',
+      '⛄',
+      '🌊',
+      '⛵',
+      '🏀',
+      '🐷',
+      '🐍',
+      '🐫',
+      '🔫',
+      '🍉',
+      '💛',
+    ]
+    return emojiList[Math.floor(Math.random() * emojiList.length)]
+  }
 
   // 從ActiveTabs拉進groups 的tab 會先關閉
   function closeTab(tabId) {
-    chrome.tabs.remove(tabId);
+    chrome.tabs.remove(tabId)
   }
 
   // 從groups 再次拉進ActiveTabs 會再次打開
   function openTab(url) {
-    chrome.tabs.create({ url: url, active: false });
+    chrome.tabs.create({ url: url, active: false })
   }
 
-  return { handleDragStart, handleDragOver, handleDrop };
-};
+  return { handleDragStart, handleDragOver, handleDrop }
+}
